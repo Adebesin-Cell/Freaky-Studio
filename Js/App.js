ac496a45d7be91290343fdf5acc1139bab74366a
adebesin-cell <adebesintolulope80@gmail.com>
2021-03-29 15:08:55 +0100

    rolling in updates

---
+++ Js/App.js
@@ -0,0 +1,9 @@
+const _nav = document.querySelector("#nav");
+const _button = document.querySelector("#toggle");
+
+const _open = () => {
+      if (true) {
+            _nav.classList.toggle("open");
+      }
+}
+_button.addEventListener("click",_open);
